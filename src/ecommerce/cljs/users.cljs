a0c8cd2faaec4aac88da3cf3588b9735069f8eb9
sgshea <sammys512@yahoo.com>
2023-04-06 19:57:59 -0400

    feat: Basic display of users, adding cljs-ajax

--- src/ecommerce/cljs/users.cljs
+++ src/ecommerce/cljs/users.cljs
@@ -1,7 +1,31 @@
-(ns ecommerce.cljs.users)
+(ns ecommerce.cljs.users
+  (:require 
+   [reagent.core :as r]
+   [ajax.core :refer [GET POST]]))
 
+(defn handler [response]
+  (.log js/console (str response)))
 
-(defn users-page
-  []
-  [:div [:h3 "Users Page"]
-   [:p "Display users"]])
+(defn error-handler [{:keys [status status-text]}]
+  (.log js/console (str "something bad happened: " status " " status-text)))
+
+(defn user-list [users]
+  [:ul
+   (for [{:keys [users/first_name users/last_name users/email]} @users]
+     [:li
+      [:p first_name " " last_name]
+      [:p email]])])
+
+(defn get-users [users]
+  (GET "/users"
+    {:headers {"Accept" "application/transit+json"}
+     :handler #(reset! users (vec %))}))
+
+(defn users-page []
+  (let [users (r/atom nil)]
+    (get-users users)
+    (fn []
+      [:div
+       [:h3 "Users"]
+       [:div
+        [user-list users]]])))
