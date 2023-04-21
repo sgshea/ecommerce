56e572d9dda73fc7c32be30f3b97ec2f24d6ebf0
sgshea <sammys512@yahoo.com>
2023-04-21 19:16:55 -0400

    feat: Reorganize menu-bar component, fix the error

--- src/ecommerce/cljs/core.cljs
+++ src/ecommerce/cljs/core.cljs
@@ -10,7 +10,6 @@
    [reagent-mui.colors :as colors]
    [reagent-mui.styles :as styles]
    [reagent-mui.material.css-baseline :refer [css-baseline]]
-   [reagent-mui.material.grid :refer [grid]]
    [reagent-mui.material.typography :refer [typography]]
    [reagent-mui.material.box :refer [box]]
    [reagent-mui.material.container :refer [container]]
@@ -20,7 +19,6 @@
    [reagent-mui.icons.brightness-4 :refer [brightness-4]]
    [reagent-mui.icons.brightness-7 :refer [brightness-7]]
    [reagent-mui.material.menu :refer [menu] :as menu-component]
-   [reagent-mui.material.menu-item :refer [menu-item]]
    [reagent-mui.icons.menu :refer [menu] :rename {menu menu-icon}]
    [reagent-mui.material.icon-button :refer [icon-button]]))
 
@@ -53,26 +51,16 @@
    {:name "Users"
     :link ::users}])
 
-(defonce anchorElNav (r/atom nil))
-
-(defn pages-item
-  [page]
-  [:a {:href (rfe/href (page :link))}
-  [menu-item {:key (page :name)
-              :on-click (fn [e]
-                         (reset! anchorElNav (event-value e)))}
-      [typography {:textAlign "center"
-                   :color "text.primary"}
-        (page :name)]]])
+(def anchorElNav (r/atom nil))
 
 (defn pages-button
   [page]
   [button {:key (page :name)
            :on-click (fn [e]
-                      (reset! anchorElNav (event-value e)))
-           :sx {:my 2
+                       (reset! anchorElNav (event-value e)))
+           :sx {:m 2
                 :color "text.secondary"
-                :display "block"}
+                :display :block}
            :href (rfe/href (page :link))}
    (page :name)]) 
 
@@ -83,17 +71,12 @@
             :color "transparent"
             :position :sticky}
    [container {:maxWidth "x1"}
-     [toolbar {:disableGutters true}
+     [toolbar
       [typography {:variant :h5
-                   :noWrap true
-                   :component :a
-                   :href "/"
                    :sx {:mr 2
                         :display {:xs "none"
                                   :md "flex"}
                         :fontFamily "monospace"
-                        :fontWeight 700
-                        :textDecoration "none"
                         :color "text.primary"}}
        "Ecommerce Web App"]
       [box {:sx {:flexGrow 1
@@ -102,35 +85,26 @@
                      :aria-label ""
                      :aria-controls "menu-appbar"
                      :aria-haspopup true
-                     :on-click (fn []
-                                (reset! anchorElNav true))
+                     :on-click (fn [e]
+                                 (reset! anchorElNav (event-value e)))
                      :color "inherit"
                      :style {:margin-left "-12px"
                              :margin-right "20px"}}
         [menu-icon]]
-       [menu {:id "menu-appbar"
-              :anchor-el anchorElNav
-              :anchorOrigin {:vertical "top"
+       [menu {:id "menu-appbar" 
+              :anchor-reference :none
+              :anchor-origin {:vertical "top"
                              :horizontal "left"}
               :keepMounted true
-              :transformOrigin {:vertical "top"
-                                :horizontal "left"}
               :open (some? @anchorElNav)
-              :onClose (fn []
-                         (reset! anchorElNav nil))
-              :sx {:display {:xs "block"
-                             :md "none"}}} 
-        (map pages-item pages)]]
+              :onClose #(reset! anchorElNav nil)}
+        (map pages-button pages)]]
       [typography {:variant :h5
                    :noWrap true
-                   :component :a
-                   :href "/"
                    :sx {:mr 2
                         :display {:xs "flex"
                                   :md "none"}
                         :fontFamily "monospace"
-                        :fontWeight 700
-                        :textDecoration "none"
                         :color "text.primary"}}
        "Ecommerce Web App"]
       [box {:sx {:flexGrow 1
@@ -139,8 +113,7 @@
        (map pages-button pages)]
       [icon-button {:sx {:m 1}
                     :color "inherit"
-                    :on-click #(toggle-dark-mode)
-                    }
+                    :on-click #(toggle-dark-mode)}
        (if (= @theme-mode :light)
          [brightness-4]
          [brightness-7])]
