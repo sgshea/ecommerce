bea5e09e7dccbe9d25354d746958f5bb4616bfd2
sgshea <sammys512@yahoo.com>
2023-05-05 15:14:13 -0400

    feat: Get datagrid update working

--- src/ecommerce/cljs/users.cljs
+++ src/ecommerce/cljs/users.cljs
@@ -4,7 +4,8 @@
    [ajax.core :refer [GET POST PUT]]
    [reagent-mui.material.button :refer [button]]
    [reagent-mui.x.data-grid :refer [data-grid]]
-   [reagent-mui.util :refer [clj->js']]))
+   [reagent-mui.util :refer [clj->js']]
+   ))
 
 (defn handler [response]
   (.log js/console (str response)))
@@ -29,33 +30,6 @@
      :params user
      :handler handler}))
 
-(def columns [{:field :id
-               :headerName "ID"
-               :width 80}
-              {:field :first-name
-               :headerName "First name"
-               :width 130}
-              {:field :last-name
-               :headerName "Last name"
-               :width 130}
-              {:field :email
-               :headerName "Email"
-               :width 200}
-              {:field :role
-               :headerName "Role"
-               :width 150}])
-
-(defn data-grid-component [rows col]
-  [:div {:style {:height 400 :width 800}}
-   [data-grid {:rows rows
-               :columns col
-               :initial-state (clj->js' {:pagination {:pagination-model {:page-size 5}}})
-               :page-size-options [5]
-               :checkbox-selection true
-               :disable-row-selection-on-click true
-               :density :standard
-               }]])
-
 (defn format-users-data [users]
   "Formats the user data from a GET request to rows for the data-table"
   (map #(hash-map :id (:users/id %)
@@ -68,14 +42,53 @@
 (defn format-user-data-back [user]
   "Formats a single user back into the format for a POST or PUT request"
   (hash-map
+   :id (:id user)
    :first_name (:first-name user)
    :last_name (:last-name user)
    :email (:email user)
-   :role_id (case user
-              "Management" 0
-              "Logistics" 1
-              "Support" 2
-              "Development" 3)))
+   :role_id (case (:role user)
+              "Management" 1
+              "Logistics" 2
+              "Support" 3
+              "Development" 4)))
+
+(defn row-update [new]
+  (put-user (format-user-data-back (js->clj new :keywordize-keys true)))
+  new)
+
+(defn row-update-error [error]
+  (.log js/console (str error)))
+
+(def columns [{:field :id
+               :headerName "ID"
+               :width 80}
+              {:field :first-name
+               :headerName "First name"
+               :width 130}
+              {:field :last-name
+               :headerName "Last name"
+               :width 130}
+              {:field :email
+               :headerName "Email"
+               :width 200
+               :editable true}
+              {:field :role
+               :headerName "Role"
+               :width 150
+               :editable true}])
+
+(defn data-grid-component [rows col]
+  [:div {:style {:height 400 :width 800}}
+   [data-grid {:rows rows
+               :columns col
+               :initial-state (clj->js' {:pagination {:pagination-model {:page-size 5}}})
+               :page-size-options [5]
+               :checkbox-selection true
+               :disable-row-selection-on-click true
+               :density :standard
+               :process-row-update row-update
+               :on-process-row-update-error row-update-error
+               }]])
 
 (defn users-page []
   "Main function defining users page"
